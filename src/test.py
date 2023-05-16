4cebe93cbea167ac66f7440252392a1e43465797
dfrws2023preprint <133549310+dfrws2023preprint@users.noreply.github.com>
2023-05-16 12:42:15 +0900

    Delete test.py

--- src/test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-match = re.match(r"(Qm[1-9A-HJ-NP-Za-km-z]{44}|baf[A-Za-z2-7]{56})", "http://bafkreibjoigvkh75ejawpl3n7lypu4x2mbeofekradwuqrop5a7bhfwk44.ipfs.dweb.link/")
-
-if match:
-    print("Match found!")
-else:
-    print("No match found.")
